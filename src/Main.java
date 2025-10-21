4a586740106f878918f3c537bb27d564e8ab11cd
Otto-Jensen <otje0002@stud.ek.dk>
2025-10-21 19:43:46 +0200

    d

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] arg){
         System.out.println("kan du se det otto");
-        System.out.println("tester lige for at se om lortet virker.");
+        System.out.println("tester lige for at se om lortet virker");
 
     }
 }
