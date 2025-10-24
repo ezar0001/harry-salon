0cc5797cd97966ca070c52e8317fd7a1fdaeddcb
ezar0001 <ezraarulampalam@MacBook-Pro-tilhrende-Ezra.local>
2025-10-24 23:24:45 +0200

    OpeningHours

--- src/Salon/OpeningHours.java
+++ src/Salon/OpeningHours.java
@@ -1,8 +1,36 @@
 package Salon;
+import java.util.LinkedHashMap;
+
 
 public class OpeningHours {
     public static void main(String[] args) {
-        System.out.println("Her laver vi OpeningHours Klassen\n Tænker bare du laver åbningstiderne (10-18) og så laver jeg ferie metoden");
-        System.out.println("okay jeg skal lige nogen ting skal hjælpe min søster med at flytte ud men jeg gøre det idag");
+OpeningHours hours = new OpeningHours();
+hours.visAabningstider();
+    }
+
+
+    private LinkedHashMap<String, String> openingHours;
+
+    //Constructor
+    public OpeningHours() {
+        openingHours = new LinkedHashMap<>();
+        opretAabningstider();
+    }
+
+    private void opretAabningstider() {
+        openingHours.put("Mandag", "10:00 - 18:00");
+        openingHours.put("Tirsdag", "10:00 - 18:00");
+        openingHours.put("Onsdag", "10:00 - 18:00");
+        openingHours.put("Torsdag", "10:00 - 18:00");
+        openingHours.put("Fredag", "10:00 - 18:00");
+        openingHours.put("Lørdag", "Lukket");
+        openingHours.put("Søndag", "Lukket");
+    }
+
+    public void visAabningstider(){
+        System.out.println("   SALONENS ÅBNINGSTIDER   ");
+        for (String dag : openingHours.keySet()){
+            System.out.println(dag + ": "+ openingHours.get(dag));
+        }
     }
 }
