0e2b9d72d5e3ca27bf633cc2fd2da2e56a7eb2e1
ezar0001 <ezraarulampalam@MacBook-Pro-tilhrende-Ezra.local>
2025-10-24 15:05:40 +0200

    ee

--- src/Salon/OpeningHours.java
+++ src/Salon/OpeningHours.java
@@ -3,5 +3,6 @@ package Salon;
 public class OpeningHours {
     public static void main(String[] args) {
         System.out.println("Her laver vi OpeningHours Klassen\n Tænker bare du laver åbningstiderne (10-18) og så laver jeg ferie metoden");
+        System.out.println("okay jeg skal lige nogen ting skal hjælpe min søster med at flytte ud men jeg gøre det idag");
     }
 }
