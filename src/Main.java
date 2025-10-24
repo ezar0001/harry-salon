dff35b14a6698079058a62fcaa2c7fac5a54aeba
ibra2604 <ibra2604@stud.ek.dk>
2025-10-24 13:05:31 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] arg){
         System.out.println("kan du se det otto");
         System.out.println("tester lige for at se om lortet virker");
+        System.out.println("hej mine venner");
 
     }
 }
