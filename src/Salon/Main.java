fe0b5e4fe9e6a00eace4dbc8a43fab6b78b6cffe
ibra2604 <ibra2604@stud.ek.dk>
2025-10-24 13:17:37 +0200

    Package Salon commit

---
+++ src/Salon/Main.java
@@ -0,0 +1,12 @@
+package Salon;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] arg){
+        System.out.println("kan du se det otto");
+        System.out.println("tester lige for at se om lortet virker");
+        System.out.println("hej mine venner");
+
+    }
+}
